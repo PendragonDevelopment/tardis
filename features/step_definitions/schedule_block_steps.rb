f4e418e6b8cfbe5835f7d4f3fe379414295aeb93
Jordan Burke <me@jordankburke.com>
2015-03-07 21:44:16 -0500

    Added preliminary step definitions

---
+++ features/step_definitions/schedule_block_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I do not have an authorized application$/) do
+  # pending # express the regexp above with the code you wish you had
+end
+
+When(/^I create a new schedule block$/) do
+  # pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I see an no authorized application error message$/) do
+  # pending # express the regexp above with the code you wish you had
+end
