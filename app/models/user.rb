db44869590daa2ecc163553152924bb1444c0ccc
Michael Bottone <bottonedev@gmail.com>
2015-03-08 16:59:15 -0400

    Woops

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   has_many :oauth_applications, class_name: 'Doorkeeper::Application', as: :owner
-
 end
