aa5f798b0b578bb2662157cd790f172a7574751b
dmcbrayer <dmcbrayer@gmail.com>
2015-03-11 09:45:40 +0800

    Added some more full featured ... features.

--- features/step_definitions/schedule_block_steps.rb
+++ features/step_definitions/schedule_block_steps.rb
@@ -9,19 +9,3 @@ end
 Then(/^I see a no authorized application error message$/) do
   @schedule_block.exists? == false
 end
-
-Given(/^I am a signed in user$/) do
-  @user.logged_in?
-end
-
-Given(/^I have no existing schedule blocks$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^I make a new schedule block$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^I should have (\d+) schedule block$/) do |arg1|
-  migrated the test db and made some schedule block feature_steps
-end
