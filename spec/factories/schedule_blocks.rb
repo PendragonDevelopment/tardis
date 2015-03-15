bc56ada513d04c8ec2f9901037597a8061d8c5a9
Jordan Burke <me@jordankburke.com>
2015-03-15 16:27:59 -0400

    Updated step definition to include Factory and changed factory names from camelCase to snake_case

--- spec/factories/schedule_blocks.rb
+++ spec/factories/schedule_blocks.rb
@@ -1,14 +1,14 @@
 FactoryGirl.define do
   factory :schedule_block do
     hostId 1
-eventId 1
-locationId 1
-startTime "2015-03-12 13:18:30"
-endTime "2015-03-12 13:18:30"
-date "2015-03-12"
-reservationMin 1
-reservationMax 1
-status 1
+    event_id 1
+    location_id 1
+    start_time "2015-03-12 13:18:30"
+    end_time "2015-03-12 13:18:30"
+    date "2015-03-12"
+    reservation_min 1
+    reservation_max 1
+    status 1
   end
 
 end
