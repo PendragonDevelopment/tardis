09a0c9cb537936df474ae45590ac9797c6b18c91
Jordan Burke <me@jordankburke.com>
2015-03-16 14:18:14 -0400

    Fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312174609) do
+ActiveRecord::Schema.define(version: 20150313204043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "appointments", force: :cascade do |t|
-    t.integer  "schedule_block_id"  null: false
-    t.integer  "attendee"           null: false
-    t.integer  "status" default: 0, null: false
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.integer  "schedule_block_id",             null: false
+    t.integer  "attendee",                      null: false
+    t.integer  "status",            default: 0, null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
   end
 
   add_index "appointments", ["schedule_block_id"], name: "index_appointments_on_schedule_block_id", using: :btree
@@ -80,7 +80,10 @@ ActiveRecord::Schema.define(version: 20150312174609) do
     t.integer  "status",          default: 0, null: false
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.integer  "user_id"
   end
+
+  add_index "schedule_blocks", ["user_id"], name: "index_schedule_blocks_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -102,4 +105,5 @@ ActiveRecord::Schema.define(version: 20150312174609) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "appointments", "schedule_blocks"
+  add_foreign_key "schedule_blocks", "users"
 end
