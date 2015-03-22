afde1b8533277a87e24ffa8a5e5bf2f5efb0d12a
JT <jtjobe@gmail.com>
2015-03-22 12:56:58 -0400

    Add presence validations on schedule_block.rb

--- app/models/schedule_block.rb
+++ app/models/schedule_block.rb
@@ -2,4 +2,7 @@ class ScheduleBlock < ActiveRecord::Base
   belongs_to :user
 	has_many :appointments
 	enum status: [ :open, :full, :reserve_not_met, :cancelled, :restricted]
+
+  validates :host_id, :event_id, :location_id, :start_time, :end_time, :reservation_min, :reservation_max, presence: true
+
 end
