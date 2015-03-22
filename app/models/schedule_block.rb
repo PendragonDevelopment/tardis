51fe051d808aded91a1f813d3ed9ed72152f462b
JT <jtjobe@gmail.com>
2015-03-22 18:22:47 -0400

    Add validations on start and end times

--- app/models/schedule_block.rb
+++ app/models/schedule_block.rb
@@ -7,4 +7,18 @@ class ScheduleBlock < ActiveRecord::Base
 
   validates :status, inclusion: { in: %w(open full reserve_not_met cancelled restricted),
     message: "%{value} is not a valid Schedule Block status." }
+
+  validates :start_time_is_a_date_time, :end_time_is_a_date_time
+
+  # Supposed to validate that end_time is after start_time
+  validates :end_time, numericality: { :greater_than => :start_time }
+
+  def start_time_is_a_date_time
+    errors.add(:start_time, "Start time is not a valid DateTime") unless :start_time.is_a?(DateTime) 
+  end
+
+  def start_time_is_a_date_time
+    errors.add(:end_time, "End time is not a valid DateTime") unless :start_time.is_a?(DateTime) 
+  end
+
 end
