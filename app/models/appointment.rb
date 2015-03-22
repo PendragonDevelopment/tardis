5817622a8d8bd6cd3a824cc3772920d2854700ea
JT <jtjobe@gmail.com>
2015-03-22 16:14:17 -0400

    Add presence validations to appointment.rb

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,7 @@
 class Appointment < ActiveRecord::Base
   belongs_to :schedule_block
   enum status: [:pending, :booked, :in_progress, :completed, :paid, :cancelled]
+
+  validates :schedule_block_id, :attendee, :status, presence: true
+
 end
