cba267f94a8b721fec519dc06708db4abeeee2c2
dmcbrayer <dmcbrayer@gmail.com>
2015-04-05 14:27:09 -0500

    Adds most of the integration tests for the api.

--- spec/factories/appointments.rb
+++ spec/factories/appointments.rb
@@ -12,17 +12,6 @@
 
 FactoryGirl.define do
 
-  factory :schedule_block do
-    host_id 1
-    event_id 1
-    location_id 1
-    start_time "2015-10-10 10:00"
-    end_time "2015-10-10 11:00"
-    reservation_min 1
-    reservation_max 1
-    status :open
-  end
-
   factory :appointment do
     attendee 1
     status 1
