f0a780a51bf28e921c2ab96c3f2283e73d18d54f
Jordan Burke <me@jordankburke.com>
2015-05-03 16:20:21 -0400

    Updated URL in Swagger initializer

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,3 +1,6 @@
 GrapeSwaggerRails.options.url      = '/swagger_doc.json'
-GrapeSwaggerRails.options.app_url  = 'http://localhost:3000'
+GrapeSwaggerRails.options.app_url  = 'https://tardis-scheduler.herokuapp.com'
 GrapeSwaggerRails.options.app_name = 'Tardis'
+GrapeSwaggerRails.options.api_auth     = 'bearer' # Or 'bearer' for OAuth
+GrapeSwaggerRails.options.api_key_name = 'Authorization'
+GrapeSwaggerRails.options.api_key_type = 'header'
