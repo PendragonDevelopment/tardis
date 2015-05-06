351c788304a7e76cf680d65fb926d7edfe2243f8
JT <jtjobe@gmail.com>
2015-05-06 14:25:02 -0400

    Added blocked to status enumerators

--- app/models/schedule_block.rb
+++ app/models/schedule_block.rb
@@ -21,11 +21,11 @@ class ScheduleBlock < ActiveRecord::Base
 
 	has_many :appointments
 
-	enum status: [ :open, :full, :reserve_not_met, :cancelled, :restricted]
+	enum status: [:open, :full, :reserve_not_met, :cancelled, :restricted, :blocked]
 
   validates :host_id, :event_id, :location_id, :start_time, :end_time, :reservation_min, :reservation_max, presence: true
 
-  validates :status, inclusion: { in: %w(open full reserve_not_met cancelled restricted), message: "%{value} is not a valid Schedule Block status." }
+  validates :status, inclusion: { in: %w(open full reserve_not_met cancelled restricted blocked), message: "%{value} is not a valid Schedule Block status." }
 
   validates_datetime :start_time, :end_time
 
